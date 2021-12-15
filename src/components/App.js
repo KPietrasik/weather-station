ec6110b4b97c5e7c1fb9a08353b1fe53e512bb78
Kamila P <kamila@lineye.pl>
2021-12-15 11:37:58 +0100

    Add weather prognose

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,98 @@
+import React from 'react';
+
 import earth from './earth.png';
 import './App.css';
+
 import StationList from './StationList'
-import React from 'react';
+import Form from './Form';
+import Result from './Result';
+
 
 
 const API = 'https://jsonplaceholder.typicode.com/users'
-
+const APIKey = 'efa2ef11f117f7485b2fca8e87a3a2f5'
 class App extends React.Component {
 
   state = {
-    stations: []
+    stations: [],
+    value: '',
+    date: '',
+    city: '',
+    sunrise: '',
+    sunset: '',
+    temp: '',
+    pressure: '',
+    wind: '',
+    err: false,
   }
 
+  handleDislpaysWeatherStations = () => {
+    fetch(API)
+    .then(response => {
 
-componentDidMount() {
- fetch(API)
-  .then(response => {
-    
-    if(response.ok) {
-      return response;
-    }
-    throw Error(response.status)
-  })
-  .then(response => response.json())
+      if(response.ok) {
+        return response;
+      }
+      throw Error(response.status)
+    })
+    .then(response => response.json())
 
-  .then(data => {
-    const station = data;
+    .then(data => {
+      const station = data;
 
-    this.setState(prevState => {
-      return { stations: prevState.stations.concat(station) }}
-      
-   ) 
-  
-  })
-    .catch(error => alert(error + "  coś nie poszło po Twojej myśli"))
+      this.setState(prevState => {
+        return { stations: prevState.stations.concat(station) }}
 
-}
+     )
+
+    })
+      .catch(error => alert(error + "  coś nie poszło po Twojej myśli"))
+
+  };
+
+
+  handleInputChange = e => {
+    this.setState({
+      value: e.target.value
+    })
+  };
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.value.length === 0) return
+    if (prevState.value !== this.state.value) {
+      const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=${APIKey}&units=metric`;
+
+      fetch(API)
+        .then(response => {
+          if (response.ok) {
+            return response
+          }
+          throw Error("Nie udało się")
+        })
+        .then(response => response.json())
+        .then(data => {
+          const time = new Date().toLocaleString()
+          this.setState(state => ({
+            err: false,
+            date: time,
+            sunrise: data.sys.sunrise,
+            sunset: data.sys.sunset,
+            temp: data.main.temp,
+            pressure: data.main.pressure,
+            wind: data.wind.speed,
+            city: state.value,
+          }));
+        })
+        .catch(err => {
+          console.log(err);
+          this.setState(prevState => ({
+            err: true,
+            city: prevState.value
+          }))
+        });
+
+    };
+
+  };
 
   render () {
     const stations = this.state.stations;
@@ -44,22 +101,29 @@ componentDidMount() {
       <div className="App">
         <header className="App-header">
           <img src={earth} className="App-logo" alt="logo" />
+          <h1>App Weather</h1>
         </header>
-  
-        <div>
-          <h1>Weather stations</h1>
-  
+        <div className="container">
+          <div className="sidebar">
+          <button className="button"onClick = {this.handleDislpaysWeatherStations}>Wyświetl dostępne stacje pogodowe</button>
           <ul>
-            {stations.length > 0 ? <StationList stations={stations}/> : <p>Aktualnie brak stacji pogodowych</p>}
-            {/* <StationList stations={stations}/> */}
+            {stations.length > 0 ? <StationList stations={stations}/> : null}
           </ul>
-          
+          </div>
+          <div className="rightbar">
+          <Form
+          value={this.state.value}
+          change={this.handleInputChange}
+        />
+        <Result weather={this.state} />
+          </div>
+
         </div>
       </div>
     );
 
   }
- 
+
 }
 
 export default App;
--- src/components/StationList.js
+++ src/components/StationList.js
@@ -1,20 +1,20 @@
 import React from 'react';
-
+import './StationList.css';
 
 const StationList = props => {
 
 const result = props.stations.map(place => (
-<li 
-style={{
-    listStyleType: "none"
-}} 
+<li className="list"
 key = {place.id}
 >
-<h4>Stacja {place.username}. Numer stacji: {place.id}.  Stacja usytuowana w {place.address.city}.  Kontakt: {place.email}</h4>
+<h4>Stacja {place.username} </h4>
+<p>Numer stacji: {place.id} </p>
+<p>Stacja usytuowana w <strong>{place.address.city}</strong></p>
+<p>Kontakt: {place.email}</p>
 </li>
 ));
 
     return result;
 }
- 
+
 export default StationList;
